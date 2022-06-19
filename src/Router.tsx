710632034633696b7305b5c8a0f17602e32e5452
Rose Kamal Love <rosekamallove@gmail.com>
2022-06-19 20:29:10 +0530

    added: Responsive Drawer

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,16 +3,19 @@ import Home from "./pages/Home";
 import SearchPatient from "./pages/SearchPatient";
 import CreatePatient from "./pages/CreatePatient";
 import ActiveVisit from "./pages/ActiveVisit";
+import ResponsiveDrawer from "./components/Drawer";
 
 function Router() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/search-patient" element={<SearchPatient />}></Route>
-        <Route path="/create-patient" element={<CreatePatient />}></Route>
-        <Route path="/active-visit" element={<ActiveVisit />}></Route>
-      </Routes>
+        <ResponsiveDrawer>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/search-patient" element={<SearchPatient />}></Route>
+            <Route path="/create-patient" element={<CreatePatient />}></Route>
+            <Route path="/active-visit" element={<ActiveVisit />}></Route>
+          </Routes>
+        </ResponsiveDrawer>
     </>
   );
 }
