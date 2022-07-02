aa71c6a7e75922f2fd53073ebafd2adba1c52242
Rose Kamal Love <rosekamallove@gmail.com>
2022-07-02 20:58:26 +0530

    added: showing results filter

--- src/pages/SearchPatient.tsx
+++ src/pages/SearchPatient.tsx
@@ -1,41 +1,79 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useSearchPatientName } from '../api/hooks/patient'
 import { Container } from '@mui/system'
-import { Grid, TextField, Typography } from '@mui/material'
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material'
 import SearchTable from '../components/SearchTable'
 import TableSkeleton from '../components/TableSekeleton'
 
 const CreatePatient = () => {
   const [name, setName] = useState<string | undefined>()
   const [id, setId] = useState<string | undefined>()
+  const [toFetch, setToFetch] = useState('100')
 
-  const result = useSearchPatientName(name, id)
+  const result = useSearchPatientName(name, id, toFetch)
 
   return (
     <Container maxWidth="xl" sx={{ marginTop: '50px' }}>
-      <Typography variant="h3">Search Patient</Typography>
-      <Grid
-        component="form"
+      <Paper
         sx={{
-          '& .MuiTextField-root': { mr: 2, mt: 2, width: '45ch' },
-          marginTop: '20px',
-          flexDirection: 'column',
+          padding: '0px',
+          marginY: '40px',
+          border: '2px solid #F59031',
+          borderRadius: '5px',
         }}
+        variant="outlined"
       >
-        <TextField
-          label="Enter Name"
-          defaultValue={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          type="number"
-          label="Enter Identifier"
-          defaultValue={id}
-          onChange={(e) => setId(e.target.value)}
-        />
-      </Grid>
-      {!result.data && (id || name) && <TableSkeleton />}
-      {result.data && <SearchTable result={result} />}
+        <Typography
+          sx={{
+            color: 'white',
+            fontWeight: 'bold',
+            background: '#F59031',
+            padding: '5px',
+          }}
+          variant="h4"
+        >
+          Search Patient
+        </Typography>
+        <Grid
+          component="form"
+          sx={{
+            margin: '20px',
+            flexDirection: 'column',
+          }}
+        >
+          <TextField
+            sx={{ width: '45ch', mt: 2, mr: 2 }}
+            label="Enter Name"
+            defaultValue={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            sx={{ width: '45ch', mt: 2, mr: 2 }}
+            type="number"
+            label="Enter Identifier"
+            defaultValue={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <TextField
+            sx={{ width: '10%', mt: 2, mr: 2 }}
+            value={toFetch}
+            type="number"
+            label="Results"
+            onChange={(e) => setToFetch(e.target.value)}
+          />
+        </Grid>
+        {!result.data && (id || name) && <TableSkeleton />}
+        {result.data && <SearchTable result={result} />}
+      </Paper>
     </Container>
   )
 }
