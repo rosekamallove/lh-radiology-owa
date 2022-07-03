e7a5fe12275d144f9140971049a12d813fbea990
Rose Kamal Love <rosekamallove@gmail.com>
2022-07-03 23:09:58 +0530

    update: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
+import { Typography } from '@mui/material'
+import { Box, Container } from '@mui/system'
 import React from 'react'
 
 const CreatePatient: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to LibreHealth</h1>
-    </div>
+    <Container maxWidth="xl" sx={{ marginTop: '50px' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img width={400} src="https://librehealth.io/img/logo.png" alt="Logo" />
+        <Typography variant="h3" color="#757270">
+          Welcome on LibreHealth Toolkit
+        </Typography>
+      </Box>
+    </Container>
   )
 }
 
